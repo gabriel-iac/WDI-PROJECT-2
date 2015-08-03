cadc58b47f279e12aeef265ca478a940ac33b0f1
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-08-03 13:57:28 +0100

    Created model for pledges and added references

--- Appstarter/app/models/project.rb
+++ Appstarter/app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :rewards
+  has_many :pledges, through: :rewards
   
 end
--- Appstarter/app/models/user.rb
+++ Appstarter/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :projects
+  has_many :rewards, through: :pledges
+  has_many :pledges
   has_secure_password
 end
