d26533817c4cd638a3651a34f133ecc8f15b613d
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-08-03 15:20:04 +0100

    created sessions controller

--- Appstarter/config/routes.rb
+++ Appstarter/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   
-
-
-
   resources :projects
   resources :users
   resources :rewards
   resources :pledges, only: [:new, :create, :destroy]
-  root 'users#index'
-
+ resources :sessions, only: [:new, :create, :destroy]
+ get "/login", to: "sessions#new"
+ delete "/logout", to: "sessions#destroy"
+ root 'users#index'
 end
