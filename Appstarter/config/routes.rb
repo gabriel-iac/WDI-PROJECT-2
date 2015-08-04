14a7886986076b74f5dcfe338c97b473f7bdfb84
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-08-04 13:27:43 +0100

    dropping comments table

--- Appstarter/config/routes.rb
+++ Appstarter/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 
-  match "/projects/add_new_comment" => "projects#add_new_comment", :as => "add_new_comment_to_projects", :via => [:project]
+  
   resources :projects
   resources :users
   resources :rewards
