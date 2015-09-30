691f5d39de08ab2962b9c9a4cbc3cc20aac9eb2b
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-09-30 14:18:39 +0100

    preparing heorku deploy

--- Appstarter/app/controllers/comments_controller.rb
+++ Appstarter/app/controllers/comments_controller.rb
@@ -15,17 +15,11 @@ class CommentsController < ApplicationController
   redirect_to "/projects/#{@comment.project.id}"
 end
 
-
-
-
-
 private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = comment.find(params[:id])
     end
-
-
 
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- Appstarter/app/controllers/projects_controller.rb
+++ Appstarter/app/controllers/projects_controller.rb
@@ -26,7 +26,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @pledge = Pledge.new( :project => @project )
-    
     @comment = Comment.new( :project => @project )
   end
 
